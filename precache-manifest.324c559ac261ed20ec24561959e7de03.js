82c3ddd00a8ab134d8d9ef586184194a4b0eb0a9
vevi1 <seunolukeye2000@gmail.com>
2020-10-29 13:34:14 +0100

    Updates

---
+++ precache-manifest.324c559ac261ed20ec24561959e7de03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda17a3505d838a5ac0caffdd16f20c2",
+    "url": "/Contact-Manager-With_React/index.html"
+  },
+  {
+    "revision": "49cf083599007d104211",
+    "url": "/Contact-Manager-With_React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7153f2afe84547eda78e",
+    "url": "/Contact-Manager-With_React/static/css/main.d7596b90.chunk.css"
+  },
+  {
+    "revision": "49cf083599007d104211",
+    "url": "/Contact-Manager-With_React/static/js/2.e59d874c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager-With_React/static/js/2.e59d874c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7153f2afe84547eda78e",
+    "url": "/Contact-Manager-With_React/static/js/main.164ec98b.chunk.js"
+  },
+  {
+    "revision": "1afef3a4502f30742762",
+    "url": "/Contact-Manager-With_React/static/js/runtime-main.261c97b6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Contact-Manager-With_React/precache-manifest.324c559ac261ed20ec24561959e7de03.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Contact-Manager-With_React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
